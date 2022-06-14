cd064378d728c06e7f50a4cf9f424b69023777a3
arunp007 <arunp4624@gmail.com>
2022-06-14 10:46:46 +0530

    Version 1

--- learnproject/learnapp/models.py
+++ learnproject/learnapp/models.py
@@ -4,7 +4,7 @@ class Signup(models.Model):
     email = models.EmailField(max_length=50)
     password = models.TextField(max_length=50)
     name = models.TextField(max_length=50)
-    age = models.IntegerField()
+    age = models.TextField(max_length=50)
     phone = models.TextField(max_length=50)
 
     class Meta:
